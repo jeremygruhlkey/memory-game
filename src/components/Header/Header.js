09575ef3367dbd10ceb99af81afc0b81da388170
Jeremy Gruhlkey <jeremygruhlkey@Naeuns-MacBook-Pro.local>
2018-04-14 21:51:04 -0700

    Adjusted styling of header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,10 @@
 import React from "react";
-
+import "./Header.css";
 const Header = (props) => (
   <header className="header">
-    <span><h1>Memory Game</h1><h2>{props.guessNotice}</h2><h2>Current score: {props.current} || High score: {props.high}</h2></span>
+    <h1>Memory Game</h1>
+    <h2>{props.guessNotice}</h2>
+    <h2>Current score: {props.current} || High score: {props.high}</h2>
   </header>
 );
 
