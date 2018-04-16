5f29271e12fa13765cb55a406734baa9bc313160
Jeremy Gruhlkey <jeremygruhlkey@Naeuns-MacBook-Pro.local>
2018-04-15 17:05:12 -0700

    Lot's of aditional styling
    added bootstrao for limited use
    added all thirty-two teams
    added nfl logo in header
    mdjusted all colors
    made header semi-translucent
    added NFL field image to header
    added team name as notification on helmet click
    added Loss alert

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,15 @@
 import React from "react";
 import "./Header.css";
 const Header = (props) => (
-  <header className="header">
+  <div>
+  <header className="header fixed-top">
+    <img className="logo" alt = "nfl logo" src = "https://officialpsds.com/imageview/r6/k5/r6k53y_large.png?1521316583"></img>
     <h1>Memory Game</h1>
-    <h2>{props.guessNotice}</h2>
-    <h2 className = "scores">Current score: {props.current} || High score: {props.high}</h2>
+    <h1>{props.guessNotice}</h1>
+    <h1 className = "scores">Current score: {props.current} || High score: {props.high}</h1>
   </header>
+  <img className="nfl-pic" alt = "nfl grass" src = "https://nesncom.files.wordpress.com/2017/01/nfl.jpg"></img>
+  </div>
 );
 
 export default Header;
